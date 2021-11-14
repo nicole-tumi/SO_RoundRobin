dcc350c84b02e8f8883303aeafb5bb931cd253ff
nicole-tumi <tuminicole@hotmail.com>
2021-11-14 16:56:25 -0500

    modificación

--- rr.py
+++ rr.py
@@ -1,69 +1,39 @@
-import random
 import heapq
 
-class Process:
+class Proceso:
     pid = 0
-    def __init__(self, priority, time):
-        self.priority = priority
-        self.time = time
+    def __init__(self, prioridad, tiempo):
+        self.prioridad = prioridad
+        self.tiempo = tiempo
         self.used = 0
-        self.pid = Process.pid
-        Process.pid += 1
+        self.pid = Proceso.pid
+        Proceso.pid += 1
     
-    def __lt__(self, other):
-        return self.priority < other.priority
+    def __Comparacion__(self, otro):
+        return self.prioridad < otro.priority
 
-
-def priority(p):
-    heapq.heapify(p)
-    current_process = heapq.heappop(p)
-    counter = 0
-    while current_process:
-        counter += 1
-        current_process.priority -= 3
-        current_process.time -= 1
-        print('\n [{}]: proceso en ejecución {}, prioridad: {}, también se necesita: {}'.format(
-            counter,
-            current_process.pid,
-            current_process.priority,
-            current_process.time
-        ))
-        for item in p:
-            print('Proceso {}, la prioridad es {}, necesita tiempo: {}'.format(
-                item.pid,
-                item.priority,
-                item.time))
-        if current_process.time != 0:
-            heapq.heappush(p, current_process)
-            heapq.heapify(p)
-        if len(p) > 0:
-            current_process = heapq.heappop(p)
-        else:
-            break
-    return counter
-
-def rotation(p):
-    rotation_time_length = 5
+def roundRobin(p):
+    quantun = 5
     current_process = p.pop(0)
-    counter = 0
+    contador = 0
     while current_process:
-        counter += 1
-        current_process.time -= 1
+        contador += 1
+        current_process.tiempo -= 1
         current_process.used += 1
-        print('\n [{}]: proceso en ejecución {}, ya ocupado: {}, todavía se necesita: {}'.format(counter,
+        print('\n [{}]: proceso en ejecución {}, ya ocupado: {}, todavía se necesita: {}'.format(contador,
             current_process.pid,
             current_process.used,
-            current_process.time
+            current_process.tiempo
         ))
         for item in p:
             print("El proceso {} aún necesita tiempo: {}".format(item.pid, item.time))
-        if current_process.time == 0:
+        if current_process.tiempo == 0:
             if len(p):
                 current_process = p.pop()
             else:
-                return counter
+                return contador
         else:
-            if current_process.used == rotation_time_length:
+            if current_process.used == quantun:
                 current_process.used = 0
                 p.append(current_process)
                 current_process = p.pop()
@@ -73,19 +43,13 @@ def leerData():
     with open("data.txt","r") as archivo:
         for linea in archivo:
             dt = linea.split(" ")
-            data.append(Process(int(dt[0]), int(dt[1])))
+            data.append(Proceso(int(dt[0]), int(dt[1])))
     return data
 
 def main():
-    method = input("\n >>> Algoritmo de programación de procesos. \nA. Algoritmo de prioridad \tB. Algoritmo de round robin \n>: ")
-    #p = [Process(random.randrange(97,100), random.randrange(1, 21)) for i in range(random.randrange(4, 9))]
+    input("Leyendo Datos...")
     p = leerData()
-    if method == 'A':
-        priority(p)
-    elif method == 'B':
-        rotation(p)
-    else:
-        print('\n [ERROR]: Error de entrada')
+    roundRobin(p)
     print()
 
 if __name__ == '__main__':
